9eb240efd003bab4e8ca9931941ba9558011f882
Nathan Chapman <hisbrothernathan@gmail.com>
2014-12-09 21:03:27 -0800

    Add comments to index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 (function () {
+  
+  // fires off when the #note element is clicked
   $('#note').on('submit', function (e) {
 
     // prevent form from default submitting
@@ -6,27 +8,32 @@
 
     // get current time
     var currentTime = new Date();
+
+    // use moment.js to format the date
     var thisTime = moment(currentTime).format('h:mm a');
 
-    // get elements
+    // get assingn elements to variables
     var $noteText = $('#note-text');
+    var $notepad = $('#notepad');
 
+    // check if the form is empty
     if ($noteText.val() === '') {
       return;
     }
 
-    var $notepad = $('#notepad');
+    // Regex through the text and format as html
+    var markdownFormatted = $noteText.val().replace(/\n/g, '<br>').replace(/(\*)(.*?)\1/g, '<b>$2</b>').replace(/(_)(.*?)\1/g, '<i>$2</i>');
 
-    var htmlFormatted = $noteText.val().replace(/\n/g, '<br>').replace(/(\*)(.*?)\1/g, '<b>$2</b>').replace(/(_)(.*?)\1/g, '<i>$2</i>');
+    // append to the article
+    $notepad.append('<section class="row"><p class="ten columns">' + markdownFormatted + '</p> <p class="two columns"><time class="note-time" datetime="'+ currentTime +'">' + thisTime + '</p></section>');
 
-    // append to the document
-    $notepad.append('<section class="row"><p class="ten columns">' + htmlFormatted + '</p> <p class="two columns"><time class="note-time" datetime="'+ currentTime +'">' + thisTime + '</p></section>');
+    // empty the textarea
     $noteText.val('');
     
+    // scroll to the bottom of the page
     window.scrollTo(0, document.body.scrollHeight);
+
+    // refocus the cursor into the textarea field
     $noteText.focus();
-  });
-  $('.note-time').on('click', function (e) {
-    console.log('I was clicked');
   });
 }());
